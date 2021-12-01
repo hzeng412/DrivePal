33747a36cfb8ff04776f740f1517ee4dbfcd7322
barixie <xiezihanbari@gmail.com>
2021-12-01 16:31:27 -0500

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by bari on 2021/12/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
